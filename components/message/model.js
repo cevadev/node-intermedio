ebbe60ee22810caf75be45730ee691df7611ede4
Carlos Eduardo Villanueva Altuna <carlos.villanueva.altuna@gmail.com>
2020-11-04 12:35:56 -0300

    Curso intermedio Nodejs

---
+++ components/message/model.js
@@ -0,0 +1,27 @@
+/** Definimos el modelo de nuestra base de datos*/
+
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+//creamos nuestro esquema pra indicarle a mongoose que tipo de info queremos almacenar
+const mySchema = new Schema({
+    user: String,
+    message: {
+        //definimos el tipo del mensaje
+        type: String,
+        //especificamos que siempre se requerira un mensaje
+        required: true,
+    },
+    date: Date,
+});
+
+/**
+ * Creamos un model que servirá siempre que querramos hacer alguna modificación dentro de la BD datos
+ * donde indicamos el esquema, y que todo lo que se cree tenga el esquema definido y se almacene
+ * en la BD con un nombre específico. Pasamos 2 parametros
+ * 1. nombre de la coleccion en mongo
+ * 2. Nuestro esquema elaborado
+ */
+const model = mongoose.model('messages', mySchema);
+
+module.exports = model;
--- components/message/store.js
+++ components/message/store.js
@@ -3,7 +3,24 @@
  * messageList -> array donde guardamos nuestros mensaje
  */
 
- const messagesList = [];
+//nos traemos a mongoose
+const db = require('mongoose');
+const model = require('./model.js');
+
+const connectionString = 
+'mongodb+srv://barcvilla:root@cluster0.nldop.mongodb.net/platzimessages_db?retryWrites=true&w=majority';
+
+//le pedimos a mongoose que utilice la promesas
+db.Promise = global.Promise;
+
+//conexion con la bd
+db.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(()=>{
+        console.info('[db] conectada con éxito');
+    })
+    .catch((error)=>{
+        console.error('[db] ', error);
+    })
 
  function addMessage(message){
      messagesList.push(message);
